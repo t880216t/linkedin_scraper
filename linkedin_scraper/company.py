7040856e38efb9a6155e1eea2211965172b16e3e
chenjian <chenjian0112@made-in-china.com>
2021-07-16 19:35:43 +0800

    update company

--- linkedin_scraper/company.py
+++ linkedin_scraper/company.py
@@ -25,9 +25,7 @@ class Company(Scraper):
     showcase_pages = []
     affiliated_companies = []
 
-    def __init__(self, linkedin_url=None, name=None, about_us=None, website=None, headquarters=None, founded=None,
-                 industry=None, company_type=None, company_size=None, specialties=None, showcase_pages=[],
-                 affiliated_companies=[], driver=None, scrape=True, get_employees=True, close_on_complete=True):
+    def __init__(self, linkedin_url=None, driver=None, scrape=True, close_on_complete=True):
         self.linkedin_url = linkedin_url
         self.token = None
         self.companyInfo = {}
@@ -45,17 +43,14 @@ class Company(Scraper):
             except:
                 driver = webdriver.Chrome()
 
-        driver.get(linkedin_url)
         self.driver = driver
 
         if scrape:
-            self.scrape(get_employees=get_employees, close_on_complete=close_on_complete)
+            self.scrape(close_on_complete=close_on_complete)
 
     # 获取cookies
-    def get_cookies(self):
-        # 获取浏览器所有Set-Cookie，返回对象是字典列表
+    def update_cookies(self):
         cookies = self.driver.get_cookies()
-        # print(f"{cookies}")
         cookie_dict = {}
         for cookie in cookies:
             if cookie['name'] == 'JSESSIONID':
@@ -63,11 +58,9 @@ class Company(Scraper):
             cookie_dict[cookie['name']] = cookie['value'].replace('"', '')
         self.cookies = cookie_dict
 
-    def scrape(self, get_employees=True, close_on_complete=True):
+    def scrape(self, close_on_complete=True):
         if self.is_signed_in():
-            self.scrape_logged_in(get_employees=get_employees, close_on_complete=close_on_complete)
-        else:
-            self.scrape_not_logged_in(get_employees=get_employees, close_on_complete=close_on_complete)
+            self.scrape_logged_in(close_on_complete=close_on_complete)
 
     def get_company_data(self, html):
         companyInfo = {}
@@ -88,7 +81,8 @@ class Company(Scraper):
                 pass
         for _data in dataList:
             # 获取公司基本信息
-            if 'request' in _data['content'].keys() and "com.linkedin.voyager.deco.organization.web.WebFullCompanyMain" in \
+            if 'request' in _data[
+                'content'].keys() and "com.linkedin.voyager.deco.organization.web.WebFullCompanyMain" in \
                     _data['content']['request']:
                 for data in dataList:
                     if data['dataId'] == _data['content']['body'] and 'data' in data['content'].keys():
@@ -123,7 +117,8 @@ class Company(Scraper):
                                 print(e)
                                 pass
             # 跟踪信息
-            elif 'request' in _data['content'].keys() and 'com.linkedin.voyager.deco.organization.web.highlights.WebHighlightItem' in \
+            elif 'request' in _data[
+                'content'].keys() and 'com.linkedin.voyager.deco.organization.web.highlights.WebHighlightItem' in \
                     _data['content']['request']:
                 for data in dataList:
                     if data['dataId'] == _data['content']['body'] and 'data' in data['content'].keys():
@@ -136,7 +131,8 @@ class Company(Scraper):
                                         'firstName': info['firstName'],
                                         'profilePicture': "{}{}".format(
                                             info['profilePicture']['rootUrl'],
-                                            info['profilePicture']['artifacts'][1]['fileIdentifyingUrlPathSegment'].replace('&amp;', '&'),
+                                            info['profilePicture']['artifacts'][1][
+                                                'fileIdentifyingUrlPathSegment'].replace('&amp;', '&'),
                                         ),
                                         'mostRecentPosition': info['mostRecentPosition'],
                                         'companyName': info['mostRecentPosition']['companyName'],
@@ -157,7 +153,8 @@ class Company(Scraper):
                                 print(e)
                                 pass
             # 推荐信息
-            elif 'request' in _data['content'].keys() and 'com.linkedin.voyager.deco.organization.web.WebSimilarCompanyCardWithRelevanceReason' in \
+            elif 'request' in _data[
+                'content'].keys() and 'com.linkedin.voyager.deco.organization.web.WebSimilarCompanyCardWithRelevanceReason' in \
                     _data['content']['request']:
                 for data in dataList:
                     if data['dataId'] == _data['content']['body'] and 'data' in data['content'].keys():
@@ -188,47 +185,14 @@ class Company(Scraper):
 
         return companyInfo
 
-
-    def scrape_logged_in(self, get_employees=True, close_on_complete=True):
+    def scrape_logged_in(self, close_on_complete=True):
         driver = self.driver
-
         driver.get(self.linkedin_url)
-
         _ = WebDriverWait(driver, 3).until(EC.presence_of_all_elements_located((By.XPATH, '//span[@dir="ltr"]')))
 
-        driver.get(self.linkedin_url)
+        self.update_cookies()
         html = driver.page_source
-        self.get_cookies()
         if html:
             self.companyInfo = self.get_company_data(html)
-
         if close_on_complete:
             driver.close()
-
-    def scrape_not_logged_in(self, close_on_complete=True, retry_limit=10, get_employees=True):
-        driver = self.driver
-        retry_times = 0
-        while self.is_signed_in() and retry_times <= retry_limit:
-            page = driver.get(self.linkedin_url)
-            retry_times = retry_times + 1
-
-        driver.get(self.linkedin_url)
-
-        if close_on_complete:
-            driver.close()
-
-    def __repr__(self):
-        _output = {}
-        _output['name'] = self.name
-        _output['about_us'] = self.about_us
-        _output['specialties'] = self.specialties
-        _output['website'] = self.website
-        _output['industry'] = self.industry
-        _output['company_type'] = self.name
-        _output['headquarters'] = self.headquarters
-        _output['company_size'] = self.company_size
-        _output['founded'] = self.founded
-        _output['affiliated_companies'] = self.affiliated_companies
-        _output['employees'] = self.employees
-
-        return json.dumps(_output).replace('\n', '')
